dc35cc531587017bd4d07540d6795fcfc1db7a67
Nilesh1927 <nileshthakurbgp@gmail.com>
2024-02-15 19:35:51 +0530

    Update tictactoe.cpp
    Updated tic-tac-toe.

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -7,18 +7,21 @@ using namespace std;
 int check_if_same(char x, char y, char z);
 int check_linear_combinations(char t);
 int check_diagonal_combinations(char t);
+int check_linear_vertical_combinations(char t);
+int check_if_filled(int pos);
 
 char matrix[3][3] = { {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };
 
 int main() {
+
 	int loopExitSignal = 0;
 	int currentPrintRow;
 	char currentPlayer = 'o';
 	int pos = 1;
 	int i;
-
+	int noBlankPos = -1;
 	int winStatus = 0;
-	char winnner;
+	char winner;
 	
 	
 
@@ -26,34 +29,48 @@ int main() {
 
 	while (!loopExitSignal) {
 		system("CLS");
-		cout << "Value of ch  = " << currentPlayer << endl;
-		cout << "Value of pos  = " << pos << endl;
 
 		currentPrintRow = 0;
 
 		for (i = 0; i < 7; i++) 
 		{
 			if (i == 2 || i == 4) {
-				cout << "----+----+---" << endl;
+				cout << "\t\t----+----+---" << endl;
 
 			}else if (i == 0 || i == 6) {
-				cout << "-------------" << endl;
+				cout << "\t\t-------------" << endl;
 
 			}
 			else {
-				cout << "| " << matrix[currentPrintRow][0]  << " | " << matrix[currentPrintRow][1] << " | " << matrix[currentPrintRow][2] << " | " << endl;
+				cout << "\t\t| " << matrix[currentPrintRow][0]  << " | " << matrix[currentPrintRow][1] << " | " << matrix[currentPrintRow][2] << " | " << endl;
 				currentPrintRow++;
 			}
 
 			
 		}
 		cout << endl << endl;
+		if(winStatus==1){
+			cout << "Player " << winner << " won!" << endl;
+			loopExitSignal=1;
 
-		cout << "Current Player : " << currentPlayer << endl;
+		}else{
+		
+		if(noBlankPos>0){
+			cout << pos << " is already filled." << endl;
+			noBlankPos=-1;
+		}
+		cout << "\x1B[34mCurrent Player : \033[0m " << currentPlayer << endl;
 
-		cout << "Enter [-1] to exit the program!" << endl;
+		cout << "Enter [-1] to exit the program" << endl;
 		cout << "Enter a position [1,2,3,4,5,6,7,8,9]:";
 		cin >> pos;
+
+		if(check_if_filled(pos)){
+			noBlankPos = pos;
+			continue;
+		}else if (pos == -1) {
+			loopExitSignal = 1;
+		}else{
 
 		switch (pos) 
 		{
@@ -87,7 +104,7 @@ int main() {
 		default:
 			break;
 		}
-
+		
 		if (currentPlayer == 'o') {
 			currentPlayer = 'x';
 		}
@@ -96,11 +113,20 @@ int main() {
 		}
 
 
-		if (pos == -1) {
-			loopExitSignal = 1;
 		}
+		
+
+		// deciding winnner & updating winStatus
+	if(check_diagonal_combinations('x') || check_linear_combinations('x') || check_linear_vertical_combinations('x')){
+		winStatus=1;
+		winner='x';
+	}else if(check_diagonal_combinations('o') || check_linear_combinations('o')|| check_linear_vertical_combinations('o')){
+		winStatus=1;
+		winner='o';
 	}
-	// deciding winnner & updating winStatus
+	}
+	}
+	
 
 
 	return 0;
@@ -152,3 +178,76 @@ int check_diagonal_combinations(char t){
 	return match;
 }
 
+int check_linear_vertical_combinations(char t){
+	int match = 0;
+	int i;
+
+	for (i = 0; i < 3; i++) {
+
+		if (matrix[0][i] == t && matrix[1][i] == t && matrix[2][i] == t) {
+			match = 1;
+		}
+	}
+
+	return match;
+}
+
+int check_if_filled(int pos)
+{	
+
+	int result = 0;
+
+	switch(pos) 
+		{
+		case 1:
+			if(matrix[0][0]=='x' || matrix[0][0]=='o'){
+				result = 1;
+			}
+			break;
+		case 2:
+			if(matrix[0][1]=='x' || matrix[0][1]=='o'){
+				result = 1;
+			}
+			break;
+		case 3:
+			if(matrix[0][2]=='x' || matrix[0][2]=='o'){
+				result = 1;
+			}
+			break;
+		case 4:
+			if(matrix[1][0]=='x' || matrix[1][0]=='o'){
+				result = 1;
+			}
+			break;
+		case 5:
+			if(matrix[1][1]=='x' || matrix[1][1]=='o'){
+				result = 1;
+			}
+			break;
+		case 6:
+			if(matrix[1][2]=='x' || matrix[1][2]=='o'){
+				result = 1;
+			}
+			break;
+		case 7:
+			if(matrix[2][0]=='x' || matrix[2][0]=='o'){
+				result = 1;
+			}
+			break;
+		case 8:
+			if(matrix[2][1]=='x' || matrix[2][1]=='o'){
+				result = 1;
+			}
+			break;
+		case 9:
+			if(matrix[2][2]=='x' || matrix[2][2]=='o'){
+				result = 1;
+			}
+			break;
+		default:
+			break;
+		}
+
+		return result;
+
+}
